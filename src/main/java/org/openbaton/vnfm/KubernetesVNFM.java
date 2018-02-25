bf24826fdb29c8db779017ea24ba68d581f6588b
carlosjgs85 <carlos.jgs85@gmail.com>
2018-02-25 22:46:29 +0100

    Added Terminate and Instantiate functions.

--- src/main/java/org/openbaton/vnfm/KubernetesVNFM.java
+++ src/main/java/org/openbaton/vnfm/KubernetesVNFM.java
@@ -12,10 +12,14 @@ import org.openbaton.catalogue.nfvo.ConfigurationParameter;
 import org.openbaton.catalogue.nfvo.Script;
 import org.openbaton.catalogue.nfvo.viminstances.BaseVimInstance;
 import org.openbaton.common.vnfm_sdk.amqp.AbstractVnfmSpringAmqp;
+import org.openbaton.common.vnfm_sdk.utils.VnfmUtils;
 import org.openbaton.exceptions.VimException;
 import org.openbaton.nfvo.vim_interfaces.resource_management.ResourceManagement;
 import org.openbaton.plugin.mgmt.PluginStartup;
 import org.openbaton.plugin.utils.RabbitPluginBroker;
+import org.openbaton.sdk.NFVORequestor;
+import org.openbaton.sdk.NfvoRequestorBuilder;
+import org.openbaton.sdk.api.exception.SDKException;
 import org.openbaton.vim.drivers.VimDriverCaller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -33,6 +37,9 @@ public class KubernetesVNFM extends AbstractVnfmSpringAmqp {
 
     private VimDriverCaller client;
 
+    public static void main(String[] args){
+        SpringApplication.run(KubernetesVNFM.class);
+    }
 
     @Override
     public VirtualNetworkFunctionRecord instantiate(VirtualNetworkFunctionRecord virtualNetworkFunctionRecord, Object scripts, Map<String, Collection<BaseVimInstance>> vimInstances) throws Exception {
@@ -41,41 +48,41 @@ public class KubernetesVNFM extends AbstractVnfmSpringAmqp {
          * Allocation of Resources
          *  the grant operation is already done before this method
          */
-//        String userdata = "";
-//        Iterator<ConfigurationParameter> configIterator = virtualNetworkFunctionRecord.getConfigurations().getConfigurationParameters().iterator();
-//        while (configIterator.hasNext()) {
-//            ConfigurationParameter configurationParameter = configIterator.next();
-//            log.debug("Configuration Parameter: " + configurationParameter);
-//            if (configurationParameter.getConfKey().equals("GROUP_NAME")) {
-//                userdata = "export GROUP_NAME=" + configurationParameter.getValue() + "\n";
-//                userdata += "echo $GROUP_NAME > /home/ubuntu/group_name.txt\n";
-//            }
-//        }
-//        userdata += getUserData();
-//
-//        log.debug("Processing allocation of Recources for vnfr: " + virtualNetworkFunctionRecord);
-//        for (VirtualDeploymentUnit vdu : virtualNetworkFunctionRecord.getVdu()) {
-//            BaseVimInstance vimInstance = vimInstances.get(vdu.getParent_vdu()).iterator().next();
-//            log.debug("Creating " + vdu.getVnfc().size() + " VMs");
-//            for (VNFComponent vnfComponent : vdu.getVnfc()) {
-//                Map<String, String> floatingIps = new HashMap<>();
-//                for (VNFDConnectionPoint connectionPoint : vnfComponent.getConnection_point()) {
-//                    if (connectionPoint.getFloatingIp() != null && !connectionPoint.getFloatingIp().equals("")) {
-//                        floatingIps.put(connectionPoint.getVirtual_link_reference(), connectionPoint.getFloatingIp());
-//                    }
-//                }
-//                try {
-//                    VNFCInstance vnfcInstance = resourceManagement.allocate(vimInstance, vdu, virtualNetworkFunctionRecord, vnfComponent, userdata, floatingIps, new HashSet<>()).get();
-//                    log.debug("Created VNFCInstance with id: " + vnfcInstance);
-//                    vdu.getVnfc_instance().add(vnfcInstance);
-//                } catch (VimException e) {
-//                    log.error(e.getMessage());
-//                    if (log.isDebugEnabled())
-//                        log.error(e.getMessage(), e);
-//                }
-//            }
-//        }
-//        log.debug("Allocated all Resources for vnfr: " + virtualNetworkFunctionRecord);
+        String userdata = "";
+        Iterator<ConfigurationParameter> configIterator =  virtualNetworkFunctionRecord.getConfigurations().getConfigurationParameters().iterator();
+        while (configIterator.hasNext()) {
+            ConfigurationParameter configurationParameter = configIterator.next();
+            log.debug("Configuration Parameter:" + configurationParameter);
+            if (configurationParameter.getConfKey().equals("GROUP_NAME")) {
+                userdata = "export GROUP_NAME=" + configurationParameter.getValue() + "\n";
+                userdata += "echo $GROUP_NAME > /home/ubuntu/group_name.txt\n";
+            }
+        }
+        userdata += getUserData();
+
+        log.debug("Processing allocation if Resources for vnfm: " + virtualNetworkFunctionRecord);
+        for (VirtualDeploymentUnit vdu : virtualNetworkFunctionRecord.getVdu()) {
+            BaseVimInstance vimInstance = vimInstances.get(vdu.getParent_vdu()).iterator().next();
+            log.debug("Creating " + vdu.getVnfc().size() + " VMs");
+            for (VNFComponent vnfComponent : vdu.getVnfc()) {
+                Map<String, String> floatingIps = new HashMap<>();
+                for (VNFDConnectionPoint connectionPoint : vnfComponent.getConnection_point()) {
+                    if (connectionPoint.getFloatingIp() != null && !connectionPoint.getFloatingIp().equals("")) {
+                        floatingIps.put(connectionPoint.getVirtual_link_reference(), connectionPoint.getFloatingIp());
+                    }
+                }
+                try {
+                    VNFCInstance vnfcInstance = resourceManagement.allocate(vimInstance, vdu, virtualNetworkFunctionRecord, vnfComponent, userdata, floatingIps, new HashSet<>()).get();
+                    log.debug("Created VNFCInstance with id: " + vnfcInstance);
+                    vdu.getVnfc_instance().add(vnfcInstance);
+                } catch (VimException e) {
+                    log.error(e.getMessage());
+                    if (log.isDebugEnabled())
+                        log.error(e.getMessage(), e);
+                }
+            }
+        }
+        log.debug("Allocated all Resources for vnfr: " + virtualNetworkFunctionRecord);
         return virtualNetworkFunctionRecord;
     }
 
@@ -117,38 +124,38 @@ public class KubernetesVNFM extends AbstractVnfmSpringAmqp {
     @Override
     public VirtualNetworkFunctionRecord terminate(VirtualNetworkFunctionRecord virtualNetworkFunctionRecord) throws Exception {
         log.info("Terminating vnfr with id " + virtualNetworkFunctionRecord.getId());
-//        NFVORequestor nfvoRequestor = new NFVORequestor("admin", "openbaton", "localhost", "8080", "1");
-//        for (VirtualDeploymentUnit vdu : virtualNetworkFunctionRecord.getVdu()) {
-//            Set<VNFCInstance> vnfciToRem = new HashSet<>();
-//            List<VimInstance> vimInstances = new ArrayList<>();
-//            VimInstance vimInstance = null;
-//            try {
-//                vimInstances = nfvoRequestor.getVimInstanceAgent().findAll();
-//            } catch (SDKException e) {
-//                log.error(e.getMessage(), e);
-//            } catch (ClassNotFoundException e) {
-//                log.error(e.getMessage(), e);
-//            }
-//            for (VimInstance vimInstanceFind : vimInstances) {
-//                if (vdu.getVimInstanceName().contains(vimInstanceFind.getName())) {
-//                    vimInstance = vimInstanceFind;
-//                    break;
-//                }
-//            }
-//            for (VNFCInstance vnfcInstance : vdu.getVnfc_instance()) {
-//                log.debug("Releasing resources for vdu with id " + vdu.getId());
-//                try {
-//                    resourceManagement.release(vnfcInstance, vimInstance);
-//                } catch (VimException e) {
-//                    log.error(e.getMessage(), e);
-//                    throw new RuntimeException(e.getMessage(), e);
-//                }
-//                vnfciToRem.add(vnfcInstance);
-//                log.debug("Released resources for vdu with id " + vdu.getId());
-//            }
-//            vdu.getVnfc_instance().removeAll(vnfciToRem);
-//        }
-//        log.info("Terminated vnfr with id " + virtualNetworkFunctionRecord.getId());
+
+        NFVORequestor nfvoRequestor = new NFVORequestor("admin", "openbaton", "default", false, "127.0.0.1", "8080", "1");
+//        NfvoRequestorBuilder nfvoRequestor = NfvoRequestorBuilder.create();
+        for (VirtualDeploymentUnit vdu : virtualNetworkFunctionRecord.getVdu()) {
+            Set<VNFCInstance> vnfciToRem = new HashSet<>();
+            List<BaseVimInstance> vimInstances = new ArrayList<>();
+            BaseVimInstance vimInstance = null;
+            try {
+                vimInstances = nfvoRequestor.getVimInstanceAgent().findAll();
+            } catch (SDKException e) {
+                log.error(e.getMessage(), e);
+            }
+            for (BaseVimInstance vimInstanceFind : vimInstances) {
+                if (vdu.getVimInstanceName().contains(vimInstanceFind.getName())) {
+                    vimInstance = vimInstanceFind;
+                    break;
+                }
+            }
+            for (VNFCInstance vnfcInstance : vdu.getVnfc_instance()) {
+                log.debug("Releasing resources for vdu with id " + vdu.getId());
+                try {
+                    resourceManagement.release(vnfcInstance, vimInstance);
+                } catch (VimException e) {
+                    log.error(e.getMessage(), e);
+                    throw new RuntimeException(e.getMessage(), e);
+                }
+                vnfciToRem.add(vnfcInstance);
+                log.debug("Released resources for vdu with id " + vdu.getId());
+            }
+            vdu.getVnfc_instance().removeAll(vnfciToRem);
+        }
+        log.info("Terminated vnfr with id " + virtualNetworkFunctionRecord.getId());
         return virtualNetworkFunctionRecord;
     }
 
@@ -159,8 +166,6 @@ public class KubernetesVNFM extends AbstractVnfmSpringAmqp {
 
     @Override
     public VirtualNetworkFunctionRecord start(VirtualNetworkFunctionRecord virtualNetworkFunctionRecord) throws Exception {
-        log.info("Starting vnfr: " + virtualNetworkFunctionRecord.getName());
-        log.info("VNFR:" + virtualNetworkFunctionRecord);
         return virtualNetworkFunctionRecord;
     }
 
@@ -194,10 +199,6 @@ public class KubernetesVNFM extends AbstractVnfmSpringAmqp {
 
     }
 
-    public static void main(String[] args) {
-        SpringApplication.run(KubernetesVNFM.class, args);
-    }
-
     @Override
     protected void setup() {
         super.setup();
@@ -207,7 +208,7 @@ public class KubernetesVNFM extends AbstractVnfmSpringAmqp {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        //Fetching the OpenstackVim using the openstack-plugin
+        //Fetching the KubernetesVIM using the kubernetes-plugin
         resourceManagement = (ResourceManagement) context.getBean("kubernetes", 19345, "15672");
         //Using the openstack-plugin directly
         client = (VimDriverCaller) ((RabbitPluginBroker) context.getBean("rabbitPluginBroker")).getVimDriverCaller("127.0.0.1", "admin", "openbaton", 5672, "kubernetes", "kubernetes", "kubernetes","15672", 300);
